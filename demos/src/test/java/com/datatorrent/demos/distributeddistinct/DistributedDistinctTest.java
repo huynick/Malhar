751f44900cc324024f8ddc61122175ed526b2475
Nicholas Huynh <nickh@datatorrent.com>
2014-08-13 15:29:47 -0700

    Added comments

--- demos/src/test/java/com/datatorrent/demos/distributeddistinct/DistributedDistinctTest.java
+++ demos/src/test/java/com/datatorrent/demos/distributeddistinct/DistributedDistinctTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.demos.distributeddistinct;
 
 import java.sql.Connection;
@@ -21,9 +36,14 @@ import com.datatorrent.api.AttributeMap;
 import com.datatorrent.api.Context.OperatorContext;
 import com.datatorrent.api.DAG;
 import com.datatorrent.lib.algo.UniqueValueCount.InternalCountOutput;
+import com.datatorrent.lib.db.jdbc.AbstractJdbcInputOperator;
+import com.datatorrent.lib.db.jdbc.AbstractJdbcTransactionableOutputOperator;
 import com.datatorrent.lib.helper.OperatorContextTestHelper;
 import com.datatorrent.lib.helper.OperatorContextTestHelper.TestIdOperatorContext;
 
+/**
+ * Test for {@link IntegerUniqueValueCountAppender} and {@link UniqueValueCountAppender}
+ */
 public class DistributedDistinctTest
 {
   private static final Logger logger = LoggerFactory.getLogger(DistributedDistinctTest.class);
